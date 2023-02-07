6dd3abd913bf1ad4b1f9f619c55e6bee8679bafb
eugene-coding <eugene.code@yandex.ru>
2023-02-07 23:37:20 +0300

    Initial commit

---
+++ Agency/Pages/Index.cshtml.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Agency.Pages;
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Broken Heart Agency
+ 
